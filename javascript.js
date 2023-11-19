d52e0911df2e84befb0a45c71183063de8a9783e
Lucas alves <lucasalvesdarocha007@gmail.com>
2023-11-19 12:34:15 -0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,11 @@
+let nome = document.getElementById("nome");
+let email = document.getElementById("email");
+let number = document.getElementById("number");
+
+function info(){
+
+  
+}
+
+let titulo = document.querySelector("#titulo");
+titulo.textContent = "novo"
